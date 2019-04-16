7872326dffe9e48bc210412abb9f424db8899ada
Mateusz Janicki <48479820+neXtcik@users.noreply.github.com>
2019-04-16 08:34:13 +0200

    Add files via upload

--- Tele-ManDesktop/Tele-ManDesktop/AdminPanel.xaml.cs
+++ Tele-ManDesktop/Tele-ManDesktop/AdminPanel.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,25 @@ namespace Tele_ManDesktop
     /// </summary>
     public partial class AdminPanel : Window
     {
-        public AdminPanel()
+        public AdminPanel(MySqlConnection con)
         {
             InitializeComponent();
+            this.con = con;
+            listUsers();
+        }
+
+        MySqlConnection con;
+
+        private void listUsers()
+        {
+            if (con.State == ConnectionState.Closed) con.Open();
+            MySqlDataAdapter dataadapter = new MySqlDataAdapter("SELECT * FROM Users", con);
+            /*DataSet dataset = new DataSet();
+            dataadapter.Fill(dataset, "Users");*/
+            DataTable dt = new DataTable("Users");
+            dataadapter.Fill(dt);
+            admingrid.ItemsSource = dt.DefaultView;
+            admingrid.Focus();
         }
     }
 }
--- Tele-ManDesktop/Tele-ManDesktop/MainWindow.xaml.cs
+++ Tele-ManDesktop/Tele-ManDesktop/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Tele_ManDesktop
 {
@@ -20,19 +22,64 @@ namespace Tele_ManDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySqlConnection con;
+        string conString = "Server=localhost;Database=teleman;Uid=root;";
         public MainWindow()
         {
             InitializeComponent();
+            connect();
+        }
+
+        private void connect()
+        {
+            con = new MySqlConnection(conString);
+            con.Open();
         }
 
         private void Okbutton_Click(object sender, RoutedEventArgs e)
         {
-            if (logintxt.Text == "admin")
+            /*if (logintxt.Text == "admin")
                 new AdminPanel().Show();
             else if (logintxt.Text == "dispatcher")
                 new DispatcherPanel().Show();
             else
-                MessageBox.Show("I am error");
+                MessageBox.Show("I am error");*/
+
+
+
+
+            string r = null;
+
+            if (con.State == ConnectionState.Closed) con.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT Rank FROM Users WHERE Login=@USERNAME AND Password=@PASSWORD", con);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@USERNAME", logintxt.Text);
+            cmd.Parameters.AddWithValue("@PASSWORD", passwordtxt.Password.ToString());
+
+            MySqlDataReader rdr = null;
+
+            rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                while (rdr.Read())
+                {
+                    r = rdr[0].ToString();
+                    if (r != null) break;
+                }
+            }
+            if (rdr != null) rdr.Close();
+
+            if (r == "Admin")
+            {
+                AdminPanel adminPanel = new AdminPanel(con);
+                adminPanel.ShowDialog();
+            }
+            else if (r == "Dispatcher")
+            {
+                DispatcherPanel dispatcherPanel = new DispatcherPanel();
+                dispatcherPanel.ShowDialog();
+            }
+            else MessageBox.Show("Wrong credentials or user is not authorized");
         }
 
         private void Exitbutton_Click(object sender, RoutedEventArgs e)
